797027e22e074f6b09854bd90b8f4ab7ba4fe401
ddsec00 <ddavlatkhojaev@gmail.com>
2026-06-05 14:14:04 +0600

    parser update (with payload)

--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,17 @@
-import struct 
+import struct
+
+
 def format_mac(mac):
     return ':'.join('%02x' % b for b in mac)
 
+
 def parse_ethernet_frame(data):
-    dest_mac, src_mac, proto = struct.unpack('!6s6sH', data[:14])
+    # Ethernet frame (14 bytes)
+    dest_mac, src_mac, proto = struct.unpack("!6s6sH", data[:14])
+
     return {
         "destination_mac": format_mac(dest_mac),
         "source_mac": format_mac(src_mac),
-        "protocol": proto
-    }    
+        "protocol": proto,
+        "payload": data[14:] 
+    }
