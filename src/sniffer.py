61e7e802fefdd270dfff6db76897a68b838249ed
ddsec00 <ddavlatkhojaev@gmail.com>
2026-06-05 14:09:05 +0600

    sniffer update

--- src/sniffer.py
+++ src/sniffer.py
@@ -1,12 +1,17 @@
 import socket
-def create_sniffer():
-    sniffer = socket.socket(socket.AF_PACKET,
-     socket.SOCK_RAW,
-    socket.ntohs(0x0003)
-      )
-    
-    
-    
-    return sniffer
 
+
+def create_sniffer(interface=None):
+    
+    sniffer = socket.socket(
+        socket.AF_PACKET,
+        socket.SOCK_RAW,
+        socket.ntohs(0x0003)
+    )
+
+    
+    if interface:
+        sniffer.bind((interface, 0))
+
+    return sniffer
 
