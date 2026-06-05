1a8af3d10350986e58f155d198ebd3a5b05b0dfc
ddsec00 <ddavlatkhojaev@gmail.com>
2026-06-05 12:57:07 +0600

    af packet instead of af inet

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from sniffer import create_sniffer
+from parser import parse_ethernet_frame
 
 def main():
     sniffer = create_sniffer()
     print("Packet sniffer started...")
     while True:
         raw_data, addr = sniffer.recvfrom(65535)
-        print(f"Packet captured: {len(raw_data)} bytes")
+        
+        eth = parse_ethernet_frame(raw_data)
+
+        print(
+            f"Src MAC: {eth['source_mac']} → "
+            f"Dst MAC: {eth['destination_mac']} | "
+            f"Proto: {eth['protocol']} | "
+            f"Size: {len(raw_data)} bytes"
+        )
 
 if __name__ == "__main__":
     main()
