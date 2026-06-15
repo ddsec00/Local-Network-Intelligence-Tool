ec5683cc8d1924fbbc55de769139f07f34effdb3
ddsec00 <ddavlatkhojaev@gmail.com>
2026-06-15 12:54:40 +0600

    changes made to main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from sniffer import create_sniffer
-from parser import parse_ethernet_frame, parse_ipv4_packet
+from parser import (
+    parse_ethernet_frame,
+    parse_ipv4_packet,
+    parse_tcp_segment,
+    parse_udp_segment
+)
 
 
 def main():
@@ -12,14 +17,43 @@ def main():
 
         eth = parse_ethernet_frame(raw_data)
 
-        # Only IPv4 packets (0x0800 = 2048)
+        # IPv4 packets only
         if eth["protocol"] == 2048:
+
             ip = parse_ipv4_packet(eth["payload"])
 
-            print(
-                f"IP {ip['source_ip']} → {ip['destination_ip']} "
-                f"| Proto: {ip['protocol']} | TTL: {ip['ttl']} | Size: {len(raw_data)}"
-            )
+            # ICMP (Ping)
+            if ip["protocol"] == 1:
+                print(
+                    f"ICMP {ip['source_ip']} → {ip['destination_ip']}"
+                )
+
+            # TCP
+            elif ip["protocol"] == 6:
+                tcp = parse_tcp_segment(ip["payload"])
+
+                print(
+                    f"TCP {ip['source_ip']}:{tcp['source_port']} "
+                    f"→ {ip['destination_ip']}:{tcp['destination_port']}"
+                )
+
+            # UDP
+            elif ip["protocol"] == 17:
+                udp = parse_udp_segment(ip["payload"])
+
+                print(
+                    f"UDP {ip['source_ip']}:{udp['source_port']} "
+                    f"→ {ip['destination_ip']}:{udp['destination_port']}"
+                )
+
+            # Any other IP protocol
+            else:
+                print(
+                    f"IP {ip['source_ip']} → {ip['destination_ip']} "
+                    f"| Proto: {ip['protocol']}"
+                )
+
+        # Non-IPv4 Ethernet frames
         else:
             print(
                 f"Ethernet | {eth['source_mac']} → {eth['destination_mac']} "
